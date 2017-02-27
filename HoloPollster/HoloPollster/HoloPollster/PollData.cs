27e561ee567b89211fa92665e1dd2835d6945882
Tarah-Peltz <tarah.peltz@colorado.edu>
2017-02-27 05:10:04 -0700

    Experimented with Android user interface

--- HoloPollster/HoloPollster/HoloPollster/PollData.cs
+++ HoloPollster/HoloPollster/HoloPollster/PollData.cs
@@ -9,23 +9,16 @@ namespace HoloPollster
     public class PollData
     {
         public string QuestionText { get; set; }
-        public string AnswerType { get; set; }
-        public string Wind { get; set; }
-        public string Humidity { get; set; }
-        public string Visibility { get; set; }
-        public string Sunrise { get; set; }
-        public string Sunset { get; set; }
+        public enum AnswerType { Default = 0, TextBox = 0, RadioButton = 1 };
+        public AnswerType type;
+
 
         public PollData()
         {
             //Because labels bind to these values, set them to an empty string to  
             //ensure that the label appears on all platforms by default.  
             this.QuestionText = "Question...";
-            this.Wind = " ";
-            this.Humidity = " ";
-            this.Visibility = " ";
-            this.Sunrise = " ";
-            this.Sunset = " ";
+            type = AnswerType.Default;
         }
 
     }
