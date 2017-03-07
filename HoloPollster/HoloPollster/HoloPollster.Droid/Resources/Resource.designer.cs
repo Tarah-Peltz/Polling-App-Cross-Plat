f63dc90b4e9c5a95ee6332800fa736f373cee35a
Tarah-Peltz <tarah.peltz@colorado.edu>
2017-03-07 00:39:06 -0700

    Added a basic Windows UI

--- HoloPollster/HoloPollster/HoloPollster.Droid/Resources/Resource.designer.cs
+++ HoloPollster/HoloPollster/HoloPollster.Droid/Resources/Resource.designer.cs
@@ -60,35 +60,35 @@ namespace HoloPollster.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050009
-			public const int autoCompleteTextView1 = 2131034121;
-			
-			// aapt resource value: 0x7f050007
-			public const int editText1 = 2131034119;
-			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
-			
-			// aapt resource value: 0x7f050003
-			public const int radioButton1 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int radioButton2 = 2131034116;
-			
-			// aapt resource value: 0x7f050005
-			public const int radioButton3 = 2131034117;
-			
-			// aapt resource value: 0x7f050002
-			public const int radioGroup1 = 2131034114;
-			
-			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			// aapt resource value: 0x7f050008
+			public const int autoCompleteTextView1 = 2131034120;
 			
 			// aapt resource value: 0x7f050006
-			public const int textView2 = 2131034118;
+			public const int editText1 = 2131034118;
 			
-			// aapt resource value: 0x7f050008
-			public const int textView3 = 2131034120;
+			// aapt resource value: 0x7f050009
+			public const int myButton = 2131034121;
+			
+			// aapt resource value: 0x7f050002
+			public const int radioButton1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int radioButton2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int radioButton3 = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int radioGroup1 = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView2 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView3 = 2131034119;
 			
 			static Id()
 			{
