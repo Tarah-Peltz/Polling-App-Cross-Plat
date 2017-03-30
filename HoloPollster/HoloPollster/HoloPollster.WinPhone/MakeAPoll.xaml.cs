868b52ca2f1ae564636aae8d3a55640eb5ae6c4d
Tarah-Peltz <tarah.peltz@colorado.edu>
2017-03-29 21:17:21 -0600

    Programmatically adding and deleting rows of stack panels, but does not add or delete in proper location

--- HoloPollster/HoloPollster/HoloPollster.WinPhone/MakeAPoll.xaml.cs
+++ HoloPollster/HoloPollster/HoloPollster.WinPhone/MakeAPoll.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace HoloPollster.WinPhone
@@ -44,18 +45,51 @@ namespace HoloPollster.WinPhone
             rowIndex += 1;
             newRow.Height = GridLength.Auto;
             newRow.MinHeight = 30;
- 
             grid.RowDefinitions.Add(newRow);
-            Adder.SetValue(Grid.RowProperty, rowIndex);
+            AdderSet.SetValue(Grid.RowProperty, rowIndex);
             StackPanel stack = new StackPanel();
             Button newBut = new Button();
             TextBox text = new TextBox();
+            Button close = new Button();
+            close.Height = 50;
+            close.Width = 50;
+            close.Content = "X";
+            close.Click += this.RemoveQuestion;
+            //close.Background.SetValue(SolidColorBrush.ColorProperty, Windows.UI.Colors.Red);
+            //close.Foreground.SetValue(SolidColorBrush.ColorProperty, Windows.UI.Colors.White);
+            close.HorizontalAlignment = HorizontalAlignment.Right;
+            close.VerticalAlignment = VerticalAlignment.Top;
             newBut.Height = 100;
             newBut.Content = "Hi!";
             Grid.SetRow(stack,rowIndex-1);
             grid.Children.Add(stack);
+            stack.Children.Add(close);
             stack.Children.Add(text);
             stack.Children.Add(newBut);
+            
+            //Name each of these programmatically and add to a list of structs
+        }
+        void RemoveQuestion(object sender, RoutedEventArgs e) {
+            rowIndex -= 1;
+            var rm = sender as Button;
+            int rmindex = (int)rm.GetValue(Grid.RowProperty);
+            foreach (UIElement control in grid.Children)
+            {
+                var usercontrol = control as StackPanel;
+                if (usercontrol != null)
+                {
+                    int childrowindex = (int)usercontrol.GetValue(Grid.RowProperty);
+                    if (childrowindex == rmindex)
+                    {
+                        grid.Children.Remove(control);
+                        grid.RowDefinitions.RemoveAt(childrowindex);
+                        break;
+                    }
+                }
+
+            }
+
+
 
         }
     }
