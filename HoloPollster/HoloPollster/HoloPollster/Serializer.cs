f07d74974679d0ad294d550d8a53e3eaf87a63ea
Tarah-Peltz <tarah.peltz@colorado.edu>
2017-04-09 19:31:42 -0600

    Added cloud and serializer classes

---
+++ HoloPollster/HoloPollster/HoloPollster/Serializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.Storage;
+
+namespace HoloPollster
+{
+    class Serializer
+    {
+
+        
+
+        
+    }
+}
