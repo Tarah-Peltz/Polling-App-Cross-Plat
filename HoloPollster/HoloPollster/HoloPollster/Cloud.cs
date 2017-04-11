16f279cded782f0bf8057635e39ae743044186d1
SumeetBatra <sscifi@gmail.com>
2017-04-10 19:06:01 -0600

    added semi-functional backend

--- HoloPollster/HoloPollster/HoloPollster/Cloud.cs
+++ HoloPollster/HoloPollster/HoloPollster/Cloud.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace HoloPollster
 {
-    class Cloud
+    public class Cloud
     {
+        public Cloud() { }
+
+        public static async Task performBlobOperation()
+        {
+            //Get storage account
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=holopollster;AccountKey=WH1IXI99gGbxO/StyP8o/KknvLLPJqJfw3YK1PNplREb2C+NDeesnJFHfmKwCldK35BMNYOyfHaHtwiksY/LVg==");
+       
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("polldata");
+
+            await container.CreateIfNotExistsAsync();
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("placeholder");
+
+            //create new blob storing serialized data
+            await blockBlob.UploadTextAsync("Hello World");
+        }
     }
 }
